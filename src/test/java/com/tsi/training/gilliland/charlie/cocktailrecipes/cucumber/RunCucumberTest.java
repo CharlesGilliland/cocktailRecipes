ad2fb9b61b89a7984f9ec92232469fcfdeae9a75
charles gilliland <charliegilliland@hotmail.co.uk>
2022-01-06 16:32:45 +0000

    Adding surefire plugin

--- src/test/java/com/tsi/training/gilliland/charlie/cocktailrecipes/cucumber/RunCucumberTest.java
+++ src/test/java/com/tsi/training/gilliland/charlie/cocktailrecipes/cucumber/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"pretty", "json:target/cucumber.json"},
+@CucumberOptions(plugin={"pretty", "json:target/cucumber.json", "html:target/cucumber"},
                 features={"src/test/resources"})
 public class RunCucumberTest{
 }
