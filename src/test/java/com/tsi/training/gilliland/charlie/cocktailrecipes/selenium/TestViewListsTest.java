035cdecf45bd652ca3f314f634829a4479602e72
charles gilliland <charliegilliland@hotmail.co.uk>
2022-01-11 14:34:12 +0000

    Removing selenium test to test jenkins

--- src/test/java/com/tsi/training/gilliland/charlie/cocktailrecipes/selenium/TestViewListsTest.java
+++ src/test/java/com/tsi/training/gilliland/charlie/cocktailrecipes/selenium/TestViewListsTest.java
@@ -5,49 +5,49 @@ package com.tsi.training.gilliland.charlie.cocktailrecipes.selenium;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-public class TestViewListsTest {
-  private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  @BeforeMethod
-  public void setUp() {
-    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("headless");
-    options.addArguments("disable-gpu");
-    WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver(options);
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-  }
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void testViewLists() {
-    driver.get("http://localhost:3000/");
-    driver.manage().window().setSize(new Dimension(1294, 1040));
-    driver.findElement(By.linkText("View")).click();
-    driver.findElement(By.linkText("Cocktails")).click();
-    driver.findElement(By.linkText("View")).click();
-    driver.findElement(By.linkText("Equipment")).click();
-    driver.findElement(By.linkText("View")).click();
-    driver.findElement(By.linkText("Ingredients")).click();
-    driver.findElement(By.linkText("View")).click();
-    driver.findElement(By.linkText("Garnish")).click();
-    driver.findElement(By.linkText("View")).click();
-    driver.findElement(By.linkText("Glass")).click();
-    driver.close();
-  }
-}
+//import org.openqa.selenium.Dimension;
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//public class TestViewListsTest {
+//  private WebDriver driver;
+//  private Map<String, Object> vars;
+//  JavascriptExecutor js;
+//  @BeforeMethod
+//  public void setUp() {
+//    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//    ChromeOptions options = new ChromeOptions();
+//    options.addArguments("headless");
+//    options.addArguments("disable-gpu");
+//    WebDriverManager.chromedriver().setup();
+//    driver = new ChromeDriver(options);
+//    js = (JavascriptExecutor) driver;
+//    vars = new HashMap<String, Object>();
+//  }
+//  @After
+//  public void tearDown() {
+//    driver.quit();
+//  }
+//  @Test
+//  public void testViewLists() {
+//    driver.get("http://localhost:3000/");
+//    driver.manage().window().setSize(new Dimension(1294, 1040));
+//    driver.findElement(By.linkText("View")).click();
+//    driver.findElement(By.linkText("Cocktails")).click();
+//    driver.findElement(By.linkText("View")).click();
+//    driver.findElement(By.linkText("Equipment")).click();
+//    driver.findElement(By.linkText("View")).click();
+//    driver.findElement(By.linkText("Ingredients")).click();
+//    driver.findElement(By.linkText("View")).click();
+//    driver.findElement(By.linkText("Garnish")).click();
+//    driver.findElement(By.linkText("View")).click();
+//    driver.findElement(By.linkText("Glass")).click();
+//    driver.close();
+//  }
+//}
